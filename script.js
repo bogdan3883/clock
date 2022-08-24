526876fd11d2aa5fc727b4c9c9118e77c9bc6c7d
Bogdan <93480175+bogdan3883@users.noreply.github.com>
2022-08-24 15:48:20 +0300

    clock done -- need buttons

---
+++ script.js
@@ -0,0 +1,24 @@
+setInterval(displayTime, 1000);
+
+
+
+function displayTime() {
+    const time = new Date();
+
+    let hours = time.getHours();
+    let minutes = time.getMinutes();
+    let seconds = time.getSeconds()
+
+    if (seconds < 10) {
+        seconds = "0" + seconds
+    }
+
+    if (minutes < 10) {
+        minutes = "0" + minutes
+    }
+
+    let timeNow = hours + ":" + minutes + ":" + seconds;
+    document.getElementById("front").innerHTML = timeNow
+}
+displayTime();
+
